70451238d2b11b168361649f8d4f8d4986349011
GeorgePAS2 <georgeplesca36@gmail.com>
2025-03-10 19:16:28 +0200

    work in progress

---
+++ Tema Lab3/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include "Math.h";
+
+using namespace std;
+
+int main() {
+	Math m;
+	printf("2+3=%d \n", m.Add(2, 3));
+	printf("2+3+4=%d \n", m.Add(2, 3, 4));
+	printf("8.4+4.5=%d \n", m.Add(8.5, 4.5));
+	printf("8.4+4.5+1.0=%d \n", m.Add(8.5, 4.5, 1.0));
+	printf("2*8=%d \n", m.Mul(2, 8));
+	printf("2*8*4=%d \n", m.Mul(2, 8, 4));
+	printf("2.5*8.4=%d \n", m.Mul(2.5, 8.4));
+	printf("2.5*8.4*4.3=%d \n", m.Mul(2.5, 8.4, 4.3));
+	printf("Suma numerelor 1, 2, 3, 4, 5 este %d \n", m.Add(5, 1, 2, 3, 4, 5));
+	printf("OOP + 2024 = %c \n", m.Add("", ""));
+	return 0;
+}
