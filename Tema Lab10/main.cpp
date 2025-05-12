41d7d5adba268cabf821b8980fde6ba0988c678d
GeorgePAS2 <georgeplesca36@gmail.com>
2025-05-12 19:39:16 +0300

    adauga problema 10 rezolvata integral

---
+++ Tema Lab10/main.cpp
@@ -0,0 +1,172 @@
+#include <iostream>
+#include <stdio.h>
+#include <exception>
+using namespace std;
+
+class div0 : public exception
+{
+	virtual const char* what() const throw()
+	{
+		return "Impartirea la 0 este imposibila!!";
+	}
+}div_zero;
+
+class exceptie_spatiu_alocat : public exception
+{
+	virtual const char* what() const throw()
+	{
+		return "Insuficient spatiu alocat pentru realizarea aceste operatii!";
+	}
+}ex_sp;
+
+class iesire_nafara : public exception
+{
+	virtual const char* what() const throw()
+	{
+		return "Operatia nu se poate realiza deoarece depasim spatiul permis!";
+	}
+}ex_iesire;
+
+template<class T>
+class Array {
+private:
+	int spatiu_alocat;
+	T* vector;
+	int current_size = 0;
+public:
+	Array() {
+		spatiu_alocat = 0;
+		vector = new T[1];
+		vector[0] = 0;
+	}
+	Array(int dimensiune) {
+		spatiu_alocat = dimensiune;
+		vector = new T[dimensiune + 1];
+	}
+	~Array() {
+		delete[] vector;
+	}
+
+	Array(const Array& Copy_Array) {
+		try {
+			if (Copy_Array.spatiu_alocat != this->spatiu_alocat) {
+				throw ex_sp;
+			}
+			this->current_size = Copy_Array.current_size;
+			for (int i = 0; i < Copy_Array.current_size; i++) {
+				this->vector[i] = Copy_Array.vector[i];
+			}
+		}
+		catch (exception& e) {
+			cout << e.what() << endl;
+		}
+	}
+
+	Array(Array&& Move_Array) {
+		this->vector = Move_Array.vector;
+		this->spatiu_alocat = Move_Array.spatiu_alocat;
+		this->current_size = Move_Array.current_size;
+
+		Move_Array.vector = nullptr;
+		Move_Array.spatiu_alocat = 0;
+		Move_Array.current_size = 0;
+	}
+
+	Array& operator+(T element) {
+		try {
+			if (current_size == spatiu_alocat) {
+				throw ex_sp;
+			}
+			vector[current_size] = element;
+			current_size++;
+		}
+		catch (exception& e) {
+			cout << e.what() << endl;
+		}
+		return *this;
+	}
+
+	Array& operator-(T element) {
+		try {
+			if (current_size == 0) {
+				throw ex_iesire;
+			}
+			for (int i = 0; i < current_size; i++) {
+				if (vector[i] == element) {
+					for (int j = i; j < current_size - 1; j++) {
+						vector[j] = vector[j + 1];
+					}
+					current_size--;
+				}
+			}
+		}
+		catch (exception& e) {
+			cout << e.what() << endl;
+		}
+		return *this;
+	}
+
+	Array& operator*(T element) {
+		for (int i = 0; i < current_size; i++) {
+			vector[i] *= element;
+		}
+		return *this;
+	}
+
+	T& operator[](int index) {
+		try {
+			if ((index < 0) || (index > current_size)) {
+				throw ex_iesire;
+			}
+		}
+		catch (exception& e) {
+			cout << e.what() << endl;
+		}
+		return vector[index];
+	}
+
+	bool find(int element) {
+		for (int i = 0; i < current_size; i++) {
+			if (vector[i] == element) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	Array& operator/(T element) {
+		try {
+			if (element==0) {
+				throw div_zero;
+			}
+			for (int i = 0; i < current_size; i++) {
+				vector[i] /= element;
+			}
+		}
+		catch (exception& e) {
+			cout << e.what() << endl;
+		}
+		return *this;
+	}
+
+
+
+	void print() {
+		for (int i = 0; i < current_size; i++) {
+			cout << vector[i] << " ";
+		}
+	}
+};
+
+	int main() {
+		Array<int> newvector(10);
+		newvector + 1 + 2 + 3 + 4 + 5 + 6 + 7 + 8 + 9 + 10 + 11;
+		newvector + 12;
+		newvector - 1 - 2 - 3;
+		newvector * 9 * 10;
+		newvector / 0;
+		newvector / 10 / 3;
+		newvector[4] = 4;
+		newvector.print();
+		return 0;
+	}
